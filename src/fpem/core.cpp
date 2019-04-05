4a9044f3a7e98d9ad9e24223707db8d1ca51f5c7
Ian Bearman <ianb@microsoft.com>
2019-04-04 19:45:04 -0700

    test updates

--- src/fpem/core.cpp
+++ src/fpem/core.cpp
@@ -3,314 +3,97 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <algorithm>
+#include <execution>
+#include <atomic>
 
 #include <limits>
 
 #include "fpcore.h"
 
-// disbale constant arithmetic warnings
+// disable constant arithmetic warnings
 #pragma warning(disable:4756)
 
 using std::cout;
 using std::endl;
 
-template<typename T>
-struct validate_base
+//
+// Validate all 16-bit div operations
+//  compute in HW at 32-bit and compare to 16-bit output
+//  tests both DIV and NARROW operations
+//
+
+template<bool, typename T> struct mint32_t;
+template <typename T> struct mint32_t<true, T> { using int_t = int; };
+template<bool, typename T> struct mint32_t;
+template <typename T> struct mint32_t<false, T> { using int_t = T; };
+
+template<typename integral_t>
+void validate_int_to_fp(integral_t a)
 {
-    static void check_binary(float a, float b, float c, float32_t x, float32_t y, float32_t z)
+    using display_t = mint32_t<(sizeof(integral_t) < sizeof(int)), integral_t>::int_t;
+
+    float16_t f = static_cast<float16_t>(a);
+    float fx = static_cast<float>(a);
+    float16_t g = static_cast<float16_t>(fx);
+
+    if (memcmp(&f, &g, sizeof(f)) != 0)
     {
-        if (memcmp((char*)&c, (char*)&z, sizeof(float)) != 0)
-        {
-            cout << "failed!" << endl;
-            cout << "x: " << a << " " << x.to_hex_string() << " " << x.to_triplet_string() << endl;
-            cout << "y: " << b << " " << y.to_hex_string() << " " << y.to_triplet_string() << endl;
+        cout << "failed!" << endl;
+        cout << "a: 0x" << std::hex << display_t(a) << endl;
+        cout << "sw_to_f16: " << (float)f << " " << f.to_hex_string() << " " << f.to_triplet_string() << endl;
+        cout << "hw_to_f32: " << (float)g << " " << g.to_hex_string() << " " << g.to_triplet_string() << endl;
 
-            float32_t z_ = c;
-            cout << "expected: " << c << " " << z_.to_hex_string() << " " << z_.to_triplet_string() << endl;
-            cout << "actual:   " << (float)z << " " << z.to_hex_string() << " " << z.to_triplet_string() << endl;
-
-            auto err = std::string{ "Failure: '" } + T::name() + "'";
-            throw std::exception(err.c_str());
-        }
+        throw std::exception("bad conv");
     }
-};
-
-struct validate_add : public validate_base<validate_add>
-{
-    static std::string name() { return "add"; }
-
-    static void validate(float a, float b)
-    {
-        float c = a + b;
-        float32_t x = a;
-        float32_t y = b;
-        float32_t z = x + y;
-
-        check_binary(a, b, c, x, y, z);
-    }
-};
-
-struct validate_sub : public validate_base<validate_sub>
-{
-    static std::string name() { return "sub"; }
-
-    static void validate(float a, float b)
-    {
-        float c = a - b;
-        float32_t x = a;
-        float32_t y = b;
-        float32_t z = x - y;
-
-        check_binary(a, b, c, x, y, z);
-    }
-};
-
-struct validate_mul : public validate_base<validate_mul>
-{
-    static std::string name() { return "mul"; }
-
-    static void validate(float a, float b)
-    {
-        float c = a * b;
-        float32_t x = a;
-        float32_t y = b;
-        float32_t z = x * y;
-
-        check_binary(a, b, c, x, y, z);
-    }
-};
-
-struct validate_div : public validate_base<validate_div>
-{
-    static std::string name() { return "div"; }
-
-    static void validate(float a, float b)
-    {
-        float c = a / b;
-        float32_t x = a;
-        float32_t y = b;
-        float32_t z = x / y;
-
-        check_binary(a, b, c, x, y, z);
-    }
-};
-
-
-template<typename T>
-void validate()
-{
-    cout << "Validating '" << T::name() << "'" << endl;
-
-    // testing all values is too slow
-    // test some suggested values
-
-    cout << "testing a few denormals...";
-
-    for (uint32_t i = 1; i < 0xff; ++i) {
-        for (uint32_t j = 0x7f; j < 0x0fff; ++j) {
-            float x = *(float *)&i;
-            float y = *(float *)&j;
-            T::validate(x, y);
-        }
-    }
-    cout << "okay!" << endl;
-
-    cout << "testing a few negative denormals...";
-    for (uint32_t i = 0x80000001; i < 0x800000ff; ++i) {
-        for (uint32_t j = 0x8000007f; j < 0x80000fff; ++j) {
-            float x = *(float *)&i;
-            float y = *(float *)&j;
-            T::validate(x, y);
-        }
-    }
-    cout << "okay!" << endl;
-
-    cout << "testing a neg+pos denormals...";
-    for (uint32_t i = 0x80000001; i < 0x800000ff; ++i) {
-        for (uint32_t j = 1; j < 0xff; ++j) {
-            float x = *(float *)&i;
-            float y = *(float *)&j;
-            T::validate(x, y);
-        }
-    }
-    cout << "okay!" << endl;
-
-    cout << "testing a pos+neg denormals...";
-    for (uint32_t i = 1; i < 0xff; ++i) {
-        for (uint32_t j = 0x80000001; j < 0x800000ff; ++j) {
-            float x = *(float *)&i;
-            float y = *(float *)&j;
-            T::validate(x, y);
-        }
-    }
-    cout << "okay!" << endl;
-
-    cout << "testing a few normals...";
-    {
-        float start = 1.0f;
-        uint32_t start_i = *(uint32_t *)&start;
-
-        for (uint32_t i = 0; i < 0xff; ++i) {
-            for (uint32_t j = 0x7f; j < 0xfff; ++j) {
-                uint32_t x_i = start_i + i;
-                uint32_t y_i = start_i + j;
-                float x = *(float *)&x_i;
-                float y = *(float *)&y_i;
-                T::validate(x, y);
-            }
-        }
-    }
-    cout << "okay!" << endl;
-
-
-    cout << "testing a few negative normals...";
-    {
-        float start1 = -1.0f;
-        float start2 = -200.0f;
-        uint32_t start_i = *(uint32_t *)&start1;
-        uint32_t start_j = *(uint32_t *)&start2;
-
-        for (uint32_t i = 0; i < 0xff; ++i) {
-            for (uint32_t j = 0x7f; j < 0xfff; ++j) {
-                uint32_t x_i = start_i + i;
-                uint32_t y_i = start_j + j;
-                float x = *(float *)&x_i;
-                float y = *(float *)&y_i;
-                T::validate(x, y);
-            }
-        }
-    }
-    cout << "okay!" << endl;
-
-    cout << "testing a neg+pos normals...";
-    {
-        float start1 = -100.0f;
-        float start2 = 100.0f;
-        uint32_t start_i = *(uint32_t *)&start1;
-        uint32_t start_j = *(uint32_t *)&start2;
-
-        for (uint32_t i = 0; i < 0xff; ++i) {
-            for (uint32_t j = 0x7f; j < 0xfff; ++j) {
-                uint32_t x_i = start_i + i;
-                uint32_t y_i = start_j + j;
-                float x = *(float *)&x_i;
-                float y = *(float *)&y_i;
-                T::validate(x, y);
-            }
-        }
-        T::validate(std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
-        T::validate(std::numeric_limits<float>::max(), -(std::numeric_limits<float>::max() / 2));
-        T::validate((std::numeric_limits<float>::max() / 2), -std::numeric_limits<float>::max());
-    }
-    cout << "okay!" << endl;
-
-    cout << "testing a pos+neg normals...";
-    {
-        float start1 = 100.0f;
-        float start2 = -100.0f;
-        uint32_t start_i = *(uint32_t *)&start1;
-        uint32_t start_j = *(uint32_t *)&start2;
-
-        for (uint32_t i = 0; i < 0xff; ++i) {
-            for (uint32_t j = 0x7f; j < 0xfff; ++j) {
-                uint32_t x_i = start_i + i;
-                uint32_t y_i = start_j + j;
-                float x = *(float *)&x_i;
-                float y = *(float *)&y_i;
-                T::validate(x, y);
-            }
-        }
-    }
-    cout << "okay!" << endl;
-
-    cout << "testing values far apart...";
-    {
-        float start1 = 1.0f;
-        float start2 = 1234556789.0f;
-        uint32_t start_i = *(uint32_t *)&start1;
-        uint32_t start_j = *(uint32_t *)&start2;
-
-        for (uint32_t i = 0; i < 0xff; ++i) {
-            for (uint32_t j = 0x7f; j < 0xfff; ++j) {
-                uint32_t x_i = start_i + i;
-                uint32_t y_i = start_j + j;
-                float x = *(float *)&x_i;
-                float y = *(float *)&y_i;
-                T::validate(x, y);
-            }
-        }
-        T::validate(std::numeric_limits<float>::max(), std::numeric_limits<float>::min());
-    }
-    cout << "okay!" << endl;
-
-    cout << "testing values really far apart...";
-    {
-        float start1 = 0.0000000001f;
-        float start2 = 1234556789.0f;
-        uint32_t start_i = *(uint32_t *)&start1;
-        uint32_t start_j = *(uint32_t *)&start2;
-
-        for (uint32_t i = 0; i < 0xff; ++i) {
-            for (uint32_t j = 0x7f; j < 0xfff; ++j) {
-                uint32_t x_i = start_i + i;
-                uint32_t y_i = start_j + j;
-                float x = *(float *)&x_i;
-                float y = *(float *)&y_i;
-                T::validate(x, y);
-            }
-        }
-    }
-    cout << "okay!" << endl;
-
-    cout << "testing values that go to infinity...";
-    T::validate(std::numeric_limits<float>::max(), std::numeric_limits<float>::max());
-    T::validate(std::numeric_limits<float>::max(), std::numeric_limits<float>::max() / 2);
-    T::validate(std::numeric_limits<float>::max(), std::numeric_limits<float>::max() / 1000);
-    T::validate(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max());
-    T::validate(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max() / 2);
-    T::validate(-std::numeric_limits<float>::max(), -std::numeric_limits<float>::max() / 1000);
-    cout << "okay!" << endl;
-
-    cout << "testing special values...";
-    {
-        float values[] = { 0.0f, 1.0f,
-            std::numeric_limits<float>::max() / 2,
-            std::numeric_limits<float>::max(),
-            std::numeric_limits<float>::min(),
-            std::numeric_limits<float>::min() / 2,
-            std::numeric_limits<float>::infinity(),
-            std::numeric_limits<float>::quiet_NaN(),
-            std::numeric_limits<float>::signaling_NaN() };
-
-
-        for (int i = 0; i < _countof(values); ++i) {
-            for (int j = 0; j < _countof(values); ++j) {
-                T::validate(values[i], values[j]);
-                T::validate(-values[i], values[j]);
-                T::validate(values[i], -values[j]);
-                T::validate(-values[i], -values[j]);
-            }
-        }
-    }
-    cout << "okay!" << endl;
 }
+
+// fill array with values for std::for_each
+uint32_t values[std::numeric_limits<uint16_t>::max() + 1];
+
+std::atomic<int> count;
+
+template<typename integral_t>
+void testall()
+{
+    cout << "testing '"<< typeid(integral_t).name() << "'";
+
+    // run through all possible values, parallelizing the outer loop
+    std::for_each(std::execution::seq, std::begin(values), std::end(values), [](uint16_t a) {
+
+        validate_int_to_fp<integral_t>(a);
+
+        // output progress
+        int old_value = count.fetch_add(1);
+        if (old_value % 10000 == 0) {
+            cout << ".";
+        }
+    });
+
+    cout << "completed\n";
+}
+
 
 int main()
 {
+    // initialize values
+    for (int i = 0; i < std::numeric_limits<uint16_t>::max(); ++i) {
+        values[i] = uint16_t(i);
+    }
+
     try
     {
-#if 1
-        //validate<validate_add>();
-        //validate<validate_sub>();
-        //validate<validate_mul>();
-        validate<validate_div>();
-#else
-        int i = 0x1;
-        int j = 0x7f;
-        float x = *(float*)&i;
-        float y = *(float*)&j;
-        validate_div::validate(x, y);
+#if 0
+        testall<int8_t>();
+        testall<uint8_t>();
+        testall<int16_t>();
+        testall<uint16_t>();
+        testall<int32_t>();
+        testall<uint32_t>();
+        testall<int64_t>();
+        testall<uint64_t>();
 #endif
+        validate_int_to_fp<int>(12345);
     }
     catch (std::exception e)
     {
